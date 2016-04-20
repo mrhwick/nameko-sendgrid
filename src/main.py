371324ac7968362ae2944cdafac7cfe182b898de
Forrest Pruitt <fpruitt0922@gmail.com>
2016-04-20 13:46:54 -0400

    Adding tests.

--- src/main.py
+++ src/main.py
@@ -10,10 +10,11 @@ class SendgridService(object):
 
 	def __init__(self):
 		self.sendgrid_key = os.environ.get('SENDGRID_API_KEY')
+		self.sendgrid_v2_client = sendgrid.SendGridClient(self.sendgrid_key)
+		self.sendgrid_v3_client = sendgrid.SendGridAPIClient(apikey=self.sendgrid_key)
 
 	@rpc
 	def send_email(self, to_email, from_email, subject, body_html, body_text):
-		sg = sendgrid.SendGridClient(self.sendgrid_key)
 		message = sendgrid.Mail(to=to_email, subject=subject, html=body_html, text=body_text, from_email=from_email)
 		print("SENDGRID: send_email: Attempting to send email to {to_email} from {from_email} with subject {subject} at time {time}".format(
 			to_email=to_email,
@@ -22,7 +23,7 @@ class SendgridService(object):
 			time=datetime.now(),
 		))
 		
-		status, msg = sg.send(message)
+		status, msg = self.sendgrid_v2_client.send(message)
 		
 		print("SENDGRID: send_email: Received response from Sendgrid at time {time} with status {status} and response {msg}".format(
 			time=datetime.now(),
@@ -31,15 +32,18 @@ class SendgridService(object):
 		))
 
 	@rpc 
-	def send_templated_email(self, to_email, from_email, subject, template_id, template_context):
-		sg = sendgrid.SendGridClient(self.sendgrid_key)
+	def send_templated_email(self, to_email, from_email, subject, template_id, template_context=None):
+		if template_context and not isinstance(template_context, dict):
+			raise ValueError("Template Context should be a dictionary of template substitutions.")
+
 		message = sendgrid.Mail(to=to_email, from_email=from_email, subject=subject, html=" ")
 		message.add_filter('templates', 'enable', '1')
 		message.add_filter('templates', 'template_id', template_id)
-		
+
 		# Add template key/value pairs
-		for arg in template_context.keys():
-			message.add_substitution(arg, template_context.get(arg))
+		if template_context:
+			for arg in template_context.keys():
+				message.add_substitution(arg, template_context.get(arg))
 		
 		print("SENDGRID: send_templated_email: Attempting to send email to {to_email} from {from_email} with subject {subject} using template {template_id} at time {time}".format(
 			to_email=to_email,
@@ -49,7 +53,7 @@ class SendgridService(object):
 			time=datetime.now(),
 		))
 		
-		status, msg = sg.send(message)
+		status, msg = self.sendgrid_v2_client.send(message)
 		
 		print("SENDGRID: send_templated_email: Received response from Sendgrid at time {time} with status {status} and response {msg}".format(
 			time=datetime.now(),
@@ -60,7 +64,6 @@ class SendgridService(object):
 
 	@rpc
 	def get_available_templates(self):
-		sg = sendgrid.SendGridAPIClient(apikey=self.sendgrid_key)
-		response = sg.client.templates.get()
+		response = self.sendgrid_v3_client.client.templates.get()
 		return json.loads(str(response.response_body, encoding='UTF-8'))
 
--- test/tests.py
+++ test/tests.py
@@ -1,6 +1,209 @@
-from nameko.rpc import RpcProxy, rpc
-from nameko.testing.services import worker_factory
+import pytest
+from unittest.mock import MagicMock, patch
+from src.main import SendgridService
 
-def test_send_email():
-    # create worker with mock dependencies
-    
+
+def test_send_email_creates_message_with_correct_data():
+	service = SendgridService()
+	sendgrid_mock = MagicMock()
+	send_method_mock = MagicMock()
+	send_method_mock.return_value = (200, 'some message')
+	sendgrid_mock.send = send_method_mock
+	service.sendgrid_v2_client = sendgrid_mock
+
+	with patch('src.main.sendgrid.Mail') as mail_mock:
+		service.send_email(
+			to_email="Test Account <test@test.com>",
+			from_email="sendertest@test.com",
+			subject="test subject",
+			body_html="test body html",
+			body_text="test body text",
+		)
+
+		mail_mock.assert_called_once_with(
+			to="Test Account <test@test.com>",
+			subject="test subject",
+			html="test body html",
+			text="test body text",
+			from_email="sendertest@test.com",
+		)
+
+
+def test_send_email_calls_sendgrid_v2_with_created_message():
+	service = SendgridService()
+	
+	sendgrid_mock = MagicMock()
+	send_method_mock = MagicMock()
+	send_method_mock.return_value = (200, 'some message')
+	sendgrid_mock.send = send_method_mock
+	service.sendgrid_v2_client = sendgrid_mock
+
+
+	with patch('src.main.sendgrid.Mail') as mail_mock:
+		return_mock = MagicMock()
+		mail_mock.return_value = return_mock
+
+		service.send_email(
+			to_email="Test Account <test@test.com>",
+			from_email="sendertest@test.com",
+			subject="test subject",
+			body_html="test body html",
+			body_text="test body text",
+		)
+
+		send_method_mock.assert_called_once_with(return_mock)
+
+def test_send_templated_email_created_message_with_correct_data():
+	service = SendgridService()
+	sendgrid_mock = MagicMock()
+	send_method_mock = MagicMock()
+	send_method_mock.return_value = (200, 'some message')
+	sendgrid_mock.send = send_method_mock
+	service.sendgrid_v2_client = sendgrid_mock
+
+	with patch('src.main.sendgrid.Mail') as mail_mock:
+		return_mock = MagicMock()
+		mail_mock.return_value = return_mock
+		service.send_templated_email(
+			to_email="Test Account <test@test.com>",
+			from_email="sendertest@test.com",
+			subject="test subject",
+			template_id="MY-FAVORITE-TEMPLATE",
+			template_context={
+				'some': 'first_value',
+				'another': 'second_value',
+			},
+		)
+
+		mail_mock.assert_called_once_with(
+			to="Test Account <test@test.com>",
+			from_email="sendertest@test.com",
+			subject="test subject",
+			html=" ",
+		)
+		return_mock.add_filter.assert_any_call(
+			'templates',
+			'enable',
+			'1'
+		)
+		return_mock.add_filter.assert_any_call(
+			'templates',
+			'template_id',
+			'MY-FAVORITE-TEMPLATE'
+		)
+		return_mock.add_substitution.assert_any_call(
+			'some',
+			'first_value',
+		)
+		return_mock.add_substitution.assert_any_call(
+			'another',
+			'second_value',
+		)
+
+def test_send_templated_email_without_template_context_creates_message():
+	service = SendgridService()
+	sendgrid_mock = MagicMock()
+	send_method_mock = MagicMock()
+	send_method_mock.return_value = (200, 'some message')
+	sendgrid_mock.send = send_method_mock
+	service.sendgrid_v2_client = sendgrid_mock
+
+	with patch('src.main.sendgrid.Mail') as mail_mock:
+		return_mock = MagicMock()
+		mail_mock.return_value = return_mock
+		service.send_templated_email(
+			to_email="Test Account <test@test.com>",
+			from_email="sendertest@test.com",
+			subject="test subject",
+			template_id="MY-FAVORITE-TEMPLATE",
+		)
+
+		mail_mock.assert_called_once_with(
+			to="Test Account <test@test.com>",
+			from_email="sendertest@test.com",
+			subject="test subject",
+			html=" ",
+		)
+		return_mock.add_filter.assert_any_call(
+			'templates',
+			'enable',
+			'1'
+		)
+		return_mock.add_filter.assert_any_call(
+			'templates',
+			'template_id',
+			'MY-FAVORITE-TEMPLATE'
+		)
+
+
+def test_send_templated_email_without_template_context_creates_message():
+	service = SendgridService()
+	sendgrid_mock = MagicMock()
+	send_method_mock = MagicMock()
+	send_method_mock.return_value = (200, 'some message')
+	sendgrid_mock.send = send_method_mock
+	service.sendgrid_v2_client = sendgrid_mock
+
+	with patch('src.main.sendgrid.Mail') as mail_mock:
+		return_mock = MagicMock()
+		mail_mock.return_value = return_mock
+		with pytest.raises(ValueError):
+			service.send_templated_email(
+				to_email="Test Account <test@test.com>",
+				from_email="sendertest@test.com",
+				subject="test subject",
+				template_id="MY-FAVORITE-TEMPLATE",
+				template_context="I don't know what I'm doing."
+			)
+
+def test_get_available_templates():
+	service = SendgridService()
+
+	expected_return_value = bytes("""{
+		"templates": [
+			{
+				"name": "Postmaster Example", 
+				"versions": [{
+					"active": 1, 
+					"updated_at": "2016-04-19 19:33:14", 
+					"id": "ca268ce9-032f-47c5-9c46-343f62ecec51", 
+					"name": "Postmaster Example Version 1", 
+					"template_id": "143380c2-a369-45a5-999f-7d344ac24e38", 
+					"subject": "<%subject%> This is a test email using a template"
+				}], 
+				"id": "143380c2-a369-45a5-999f-7d344ac24e38"
+			}
+		]
+	}""".strip('\n\t'), encoding='UTF-8')
+
+	response = MagicMock()
+	response.response_body = expected_return_value
+
+	service.sendgrid_v3_client = MagicMock()
+	service.sendgrid_v3_client.client.templates.get = MagicMock(return_value=response)
+	response = service.get_available_templates()
+	assert(type(response) is dict)
+	assert('templates' in response.keys())
+	assert(len(response['templates']) == 1)
+	assert(response['templates'][0]['name'] == "Postmaster Example")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
